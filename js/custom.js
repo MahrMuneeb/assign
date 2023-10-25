3a2a1f458e9eb1436e0ea628d30917645c9f3d15
MahrMuneeb <muneebmahr29@gmail.com>
2023-10-25 21:55:52 +0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,25 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    responsiveClass:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:true
         },
         600:{
-            items:1
+            items:2,
+            nav:true
         },
         1000:{
-            items:1
+            items:4,
+            nav:true,
+            loop:false
+        },
+        1400:{
+            items:4,
+            nav:true,
+            loop:false
         }
     }
 })
